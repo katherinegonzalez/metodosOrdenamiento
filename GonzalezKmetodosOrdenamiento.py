d338838f4cd0081ca4eecb93b8fa88952f6df2d2
katherinex <kathexio@gmail.com>
2022-09-25 21:26:47 -0500

    Métodos de Ordenamiento: Burbuja

---
+++ GonzalezKmetodosOrdenamiento.py
@@ -0,0 +1,152 @@
+# ----------------------------------------------------------------------------------------
+# MODULO: Métodos de Ordenamiento
+# ----------------------------------------------------------------------------------------
+# Descripción: En este módulo se encuentran las funciones de los métodos de ordenamiento:
+# Burbuja, Seleccion e Inserción, adicionalmente se encuentran otras funciones auxiliares
+# para realziar valdiaciones y generar la lista aleatoria.
+# ----------------------------------------------------------------------------------------
+# Autor: Katherine Xiomar González Santacruz
+# Version: 1.0
+# [25.09.2022]
+# ----------------------------------------------------------------------------------------
+# IMPORTAR MODULOS
+from random import randint
+# ----------------------------------------------------------------------------------------
+# FUNCIÓN: validarInput
+# ----------------------------------------------------------------------------------------
+# Descripción: función auxiliar para validar el valor ingresado por el usuario
+# ----------------------------------------------------------------------------------------
+# PARÁMETROS & PRE-CONDICIONES
+#       Variables de entrada: (str) mensaje, (function) condicion
+#       variable auxiliar: (bool) seguirPreguntando
+# ----------------------------------------------------------------------------------------
+# VALOR DE RETORNO & POSTCONDICIONES
+#       PostCondiciones:
+#           Si seguir preguntando es false, el ciclo se rompe y se retorna el valor ingresado
+#           por el usuario.
+#           Si la condición no se cumple imprime un mensaje en pantalla
+#       Valor de retorno: (str) opcionIngresada
+# ----------------------------------------------------------------------------------------
+def validarInput (mensaje, condicion):
+    seguirPreguntando = True
+    while seguirPreguntando:
+        opcionIngresada = input(mensaje)
+        if (condicion(opcionIngresada)):
+            seguirPreguntando = False
+        else:
+            print('El valor ingresado no es válido')
+    print('opcionIngresada: ', opcionIngresada)
+    return opcionIngresada
+
+# ----------------------------------------------------------------------------------------
+# FUNCIÓN: esNumero
+# ----------------------------------------------------------------------------------------
+# Descripción: función auxiliar para validar si el valor recibido es un número
+# ----------------------------------------------------------------------------------------
+# PARÁMETROS & PRE-CONDICIONES
+#       Variables de entrada: (str) número
+# ----------------------------------------------------------------------------------------
+# VALOR DE RETORNO & POSTCONDICIONES
+#       PostCondiciones:
+#           Si es numero retorna True, de lo contrario retorna False
+#       Valor de retorno: bool (True o False)
+# ----------------------------------------------------------------------------------------
+def esNumero(numero):
+    return numero.isdigit()
+
+# ----------------------------------------------------------------------------------------
+# FUNCIÓN: longitudIngresada
+# ----------------------------------------------------------------------------------------
+# Descripción: función auxiliar para capturar la longitud de la lista ingresada por el usuario y
+# convertirla en entero
+# ----------------------------------------------------------------------------------------
+# PARÁMETROS & PRE-CONDICIONES
+#       Variables de entrada: (str) mensaje (mensaje a mostrar en el input)
+# ----------------------------------------------------------------------------------------
+# VALOR DE RETORNO & POSTCONDICIONES
+#       Valor de retorno: int (numero)
+# ----------------------------------------------------------------------------------------
+def longitudIngresada(mensaje):
+    numero = validarInput(mensaje, esNumero)
+    print('numero: ', numero)
+    return int(numero)
+
+# ----------------------------------------------------------------------------------------
+# FUNCIÓN: validarNumeroRandom
+# ----------------------------------------------------------------------------------------
+# Descripción: función auxiliar para generar y validar cada uno de los números de una lista
+# aleatoria y que no se repita dentro de la lista
+# ----------------------------------------------------------------------------------------
+# PARÁMETROS & PRE-CONDICIONES
+#       Variables de entrada: (lista) lista
+#       variable auxiliar: (bool) seguirgenerandoNum
+# ----------------------------------------------------------------------------------------
+# VALOR DE RETORNO & POSTCONDICIONES
+#       PostCondiciones:
+#           Si seguirgenerandoNum es false, el ciclo se rompe y se retorna el número random
+#           generado
+#           Si la condición no se cumple vuelve a generar y a validar otro número aleatorio
+#       Valor de retorno: (str) numero
+# ----------------------------------------------------------------------------------------
+def validarNumeroRandom (lista):
+    seguirgenerandoNum = True
+    while seguirgenerandoNum:
+        numero = randint(0, 9)
+        if numero not in lista:
+            seguirgenerandoNum = False
+    return numero
+
+# ----------------------------------------------------------------------------------------
+# FUNCIÓN: listaAleatoria
+# ----------------------------------------------------------------------------------------
+# Descripción: función para generar una lista con N números aleatorios del 0 al 9 que no se repitan
+# ----------------------------------------------------------------------------------------
+# PARÁMETROS & PRE-CONDICIONES
+#       (list) listaRandom
+#       (int) longitud
+# ----------------------------------------------------------------------------------------
+# VALOR DE RETORNO & POSTCONDICIONES
+#       PostCondiciones:
+#           se retorna una lista con N números aleatorios
+#       Valor de retorno: (lista) listaRandom
+# ----------------------------------------------------------------------------------------
+def listaAleatoria(longitud):
+    print('longitud: ', longitud)
+    listaRandom = []
+    for i in range(longitud):
+        num = validarNumeroRandom(listaRandom)
+        listaRandom.append(num)
+    return listaRandom
+
+# ----------------------------------------------------------------------------------------
+# FUNCIÓN: burbuja
+# ----------------------------------------------------------------------------------------
+# Descripción: función que ordena una lista aleatoria con el método Burbuja
+# ----------------------------------------------------------------------------------------
+# PARÁMETROS & PRE-CONDICIONES
+#       Variables de entrada: (int) longitusLista
+# ----------------------------------------------------------------------------------------
+# VALOR DE RETORNO & POSTCONDICIONES
+#       PostCondiciones:
+#           Se imprime la lista ordenada
+# ----------------------------------------------------------------------------------------
+def burbuja(longitudLista):
+    print('Método Burbuja: \n')
+    lista = listaAleatoria(longitudLista)
+    print('Lista Desordenada: ', lista)
+    sigueDesordenado = True
+    while sigueDesordenado:
+        sigueDesordenado = False
+        for i in range(len(lista) - 1):
+            if (lista [i] > lista [i + 1]):
+                elementoAnterior = lista[i]
+                lista[i] = lista[i+1]
+                lista[i+1] = elementoAnterior
+                sigueDesordenado = True
+
+    print('Lista Ordenada: ', lista)
+
+
+# ----------------------------------------------------------------------------------------
+# end.
+# ----------------------------------------------------------------------------------------
---
+++ GonzalezKordenarListas.py
@@ -0,0 +1,79 @@
+# ----------------------------------------------------------------------------------------
+# PROGRAMA: Métodos de ordenamiento
+# ----------------------------------------------------------------------------------------
+# Descripción: Este programa presenta un menú con 3 tipos de ordenamiento: Burbuja,
+# Seleccion e Inserción. El programa se encarga de generar una lista aleatoria y ordenarla
+# con el método de ordenamiento que el usuario seleccione. EL usuario debe ingresar
+# úncamente la longitud de la lista.
+
+# ----------------------------------------------------------------------------------------
+# Autor: Katherine Xiomar González Santacruz
+# Version: 1.0
+# [25.09.2022]
+# ----------------------------------------------------------------------------------------
+# IMPORTAR MODULOS
+from GonzalezKmetodosOrdenamiento import *
+
+# ----------------------------------------------------------------------------------------
+# VARIABLES GLOBALES Y PRE-CONDICIONES
+# ----------------------------------------------------------------------------------------
+
+# Variables de entrada: (str) opcionIngresada
+# pre-condiciones: opcionIngresada debe ser '1', '2', '3', ó '0'
+#                  opcionIngresada != '0'
+
+# Variables auxiliares: (str) opcion
+#                       opcion != '0'
+#
+# Explicación: opción es una variable auxiliar que guarda la opción ingresada por el usuario,
+#              y se encarga de detener el ciclo cuando la opción seleccionada es '0'.
+#              Si opcion es '0', el programa dejará de ejecutarse.
+#              Si opcionIngresada es '1', se ejecuta la función burbuja() del módulo importado
+#              Si opcionIngresada es '2', se ejecuta la función seleccion() del módulo importado
+#              Si opcionIngresada es '3', se ejecuta la función insercion() del módulo importado
+#              Si opcionIngresada es '0', se detiene el programa.
+#
+# ----------------------------------------------------------------------------------------
+# POSTCONDICIONES
+# ----------------------------------------------------------------------------------------
+# Salida: Mensaje (str) mostrando la lista aleatoria generada en desorden y la lista ordenada
+# con el método de ordenamiento que seleccionó el usuario.
+
+# Si opcionIngresada es un valor diferente de '0', '1', '2', ó 3 mostrar un mensaje
+# en pantalla, y solicitar el valor nuevamente.
+# Si opcion == '0' , fin del programa.
+
+# ----------------------------------------------------------------------------------------
+
+opcion = ''
+longitudLista = 10
+
+def condicionInput (opcionIngresada):
+    return opcionIngresada == '1' or opcionIngresada == '2' or \
+           opcionIngresada == '3' or opcionIngresada == '0'
+
+
+while (opcion != '0'):
+    print('\n')
+    print('Programa Ordenar Elementos de una Lista generada aleatoriamente.')
+    longitudLista = longitudIngresada('Ingrese la longitud de la lista que desea ordenar: ')
+    print('Ahora seleccione método de ordenamiento: ')
+    print('1. Burbuja')
+    print('2. Selección')
+    print('3. Inserción')
+    print('0. Salir')
+    opcion = validarInput('Ingrese opción: ', condicionInput)
+
+    print('\n')
+    if (opcion == '1'):
+        burbuja(longitudLista)
+    elif (opcion == '2'):
+        print('yaa')
+        # seleccion()
+    elif (opcion == '3'):
+        print('yaa')
+        # insercion()
+
+# ----------------------------------------------------------------------------------------
+# end.
+# ----------------------------------------------------------------------------------------
